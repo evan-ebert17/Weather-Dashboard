2421a2e84885380493ec7c23bd121cd4c8be8e49
EvanEbert <evan.ebert17@gmail.com>
2021-08-16 13:12:20 -0500

    finished functionality, just needs 'pretty' css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,7 +12,12 @@ let coordinate = {
 let cityName;
 let weatherData;
 let dailyForecast;
+// var today = new Date();
+// var dd = String(today.getDate()).padStart(2, '0');
+// var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+// // var yyyy = today.getFullYear();
 
+// today = mm + '/' + dd; //+ '/' + yyyy;
 
 searchButton.addEventListener('click', function (event) {
     event.preventDefault();
@@ -27,20 +32,26 @@ searchButton.addEventListener('click', function (event) {
 
 
 
-function searchWeather () {
-fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.lon}&exclude=alerts&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`)
+function searchWeather() {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.lon}&exclude=alerts&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            appendWeather(data.current);
+        })
+}
 
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        appendWeather(data.current);
-        
-    })
-    // .then(data => {
-    //     console.log(data.daily)
-    // })
+function searchForecast() {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.lon}&exclude=alerts&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            appendForecast(data.daily);
+        })
 }
 
 
@@ -57,15 +68,14 @@ function geoCoords(city) {
             coordinate.lat = data[0].lat
             coordinate.lon = data[0].lon
             searchWeather();
+            searchForecast();
         })
 }
 
 
 function appendWeather(weatherData) {
-    //apend
     weatherBox.innerHTML = '';
     let cityNameEl = document.createElement("h2");
-    // let dayDate = document.createElement("h2");
     let weatherTemp = document.createElement("p");
     let weatherWind = document.createElement("p");
     let weatherUv = document.createElement("p");
@@ -74,6 +84,7 @@ function appendWeather(weatherData) {
     weatherWind.textContent = 'Wind Speed: ' + weatherData.wind_speed;
     weatherUv.textContent = 'UV: ' + weatherData.uvi;
     weatherHumid.textContent = 'Humidity: ' + weatherData.humidity;
+    weatherUv.classList.add('uvbox')
     cityNameEl.textContent = cityName;
     weatherBox.append(cityNameEl);
     cityNameEl.appendChild(weatherTemp);
@@ -82,25 +93,36 @@ function appendWeather(weatherData) {
     cityNameEl.appendChild(weatherUv);
 };
 
-// function appendForecast(dailyForecast) {
-//     // let cityNameEl5 = document.createElement("h2");
-//     // let dayDate = document.createElement("h2");
-//     let weatherTemp5 = document.createElement("p");
-//     let weatherWind5 = document.createElement("p");
-//     let weatherUv5 = document.createElement("p");
-//     let weatherHumid5 = document.createElement("p");
-//     weatherTemp5.textContent = dailyForecast.temp;
-//     weatherWind5.textContent = dailyForecast.wind_speed;
-//     weatherUv5.textContent = dailyForecast.uvi;
-//     weatherHumid5.textContent = dailyForecast.humidity;
-//     // cityNameEl5.textContent = cityName;
-//     forecastBox.append(weatherTemp5);
-//     forecastBox.append(weatherWind5);
-//     forecastBox.append(weatherHumid5);
-//     forecastBox.append(weatherUv5);
-
-// }
+function appendForecast(dailyForecast) {
+    forecastBox.innerHTML = '';
+    var textCast = document.getElementById('forecastText');
+    var forecastTextDiv = document.getElementById('headerTextDiv');
+    forecastTextDiv.style.display = 'inline';
+    textCast.style.display = 'inline';
+    for (let i = 0; i < dailyForecast.length; i++) {
+        if (i < 5) {
+            let weatherCard = document.createElement('div');
+            weatherCard.classList.add('weathercard')
+            weatherCard.innerHTML = '';
+            let weatherTemp5 = document.createElement("h3");
+            let weatherWind5 = document.createElement("h3");
+            let weatherUv5 = document.createElement("h3");
+            let weatherHumid5 = document.createElement("h3");
+            weatherUv5.classList.add('uvbox');
+            weatherTemp5.textContent = 'Temperature: ' + dailyForecast[i].temp.day;
+            weatherWind5.textContent = 'Wind Speed: ' + dailyForecast[i].wind_speed;
+            weatherUv5.textContent = 'UV: ' + dailyForecast[i].uvi;
+            weatherHumid5.textContent = 'Humidity: ' + dailyForecast[i].humidity;
+            forecastBox.append(weatherCard);
+            //weatherCard.append(today);
+            weatherCard.append(weatherTemp5);
+            weatherCard.append(weatherWind5);
+            weatherCard.append(weatherHumid5);
+            weatherCard.append(weatherUv5);
+        }
+    }
+}
 
 function appendCityButtons(params) {
-    
+
 }
